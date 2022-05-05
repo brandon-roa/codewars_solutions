b2723fae1efe84cad282125d985f21db7bbc5fe1
Brandon Roa <brandon.g.roa@gmail.com>
2022-05-04 21:56:17 -0700

    previously completed kata

---
+++ 8ku/countPositivesSumNegatives.js
@@ -0,0 +1,26 @@
+/* Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+Example
+
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(input) {
+    let positives = 0
+    let negatives = 0
+    //check if array is null or empty and return empty array if so
+    if(input===null || input.length === 0){
+        return([])
+    }else{
+    input.forEach(x=>{
+      if((Math.sign(x))===1){
+        positives += 1
+      } else {
+        negatives += x
+      }
+    })}
+    return([positives,negatives])
+  }
